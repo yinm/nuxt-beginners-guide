bd166194db4b0afee6528b8db3dd78126312160e
yinm <yinm.cminor9@gmail.com>
2018-12-05 08:37:56 +0900

    Add posts page

---
+++ packages/nuxt-blog-service/app/pages/posts/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="container posts-page">
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>新規投稿</span>
+      </div>
+      <el-table
+        :data="showPosts"
+        style="width: 100%;"
+        class="table"
+      >
+        <el-table-column
+          prop="title"
+          label="タイトル">
+        </el-table-column>
+        <el-table-column
+          prop="user.id"
+          label="投稿者"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="created_at"
+          label="投稿日時"
+          width="240">
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </section>
+</template>
+
+ <script>
+ export default {
+   computed: {
+     showPosts() {
+       return [
+         {
+           id: '001',
+           title: 'Now to development',
+           body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+           created_at: '2018/08/10 12:00:00',
+           user: {
+             id: 'potato4d'
+           }
+         },
+         {
+           id: '002',
+           title: 'development Nuxt.js',
+           body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+           created_at: '2018/08/10 13:00:00',
+           user: {
+             id: 'potato4d'
+           }
+         }
+       ]
+     }
+   }
+ }
+ </script>
+
+<style>
+.posts-page .el-table__row {
+  cursor: pointer;
+}
+</style>
