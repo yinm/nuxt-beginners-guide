5dd89e7a7d05f915a2a924cf9d858183cfe32a73
yinm <yinm.cminor9@gmail.com>
2018-12-04 09:08:41 +0900

    Add authn pages

--- packages/nuxt-api-examples/pages/index.vue
+++ packages/nuxt-api-examples/pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <h1>Index page</h1>
+    <h2>index page</h2>
+    <ul>
+      <li>
+        <nuxt-link to="/login">ログインページ</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/authed-route">認証が必要なページ</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
