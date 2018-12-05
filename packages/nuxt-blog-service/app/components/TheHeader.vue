fc957d3858b0456c53bf38d68eb7f74852bcc263
yinm <yinm.cminor9@gmail.com>
2018-12-05 09:38:22 +0900

    Add user info

--- packages/nuxt-blog-service/app/components/TheHeader.vue
+++ packages/nuxt-blog-service/app/components/TheHeader.vue
@@ -7,13 +7,25 @@
       投稿一覧
     </el-menu-item>
 
-    <no-ssr>
-      <el-menu-item index="4" style="float: right;" :route="{ path: '/' }">
-        <span>ログイン</span>
-      </el-menu-item>
-    </no-ssr>
+    <el-menu-item index="4" style="flat: right;"
+      :route="{ path: `/users/${user.id}` }" v-if="user">
+      <span>{{ user.id }}</span>
+    </el-menu-item>
+    <el-menu-item index="4" style="float: right;" :route="{ path: '/' }" v-else>
+      <span>ログイン</span>
+    </el-menu-item>
     <el-menu-item index="5" style="float: right;" :route="{ path: '/posts/new' }">
       新規投稿
     </el-menu-item>
   </el-menu>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['user'])
+  }
+}
+</script>
