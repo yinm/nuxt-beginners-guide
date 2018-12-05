481ac1c23e19e9420f7af43cd7c0d2e1656053db
yinm <yinm.cminor9@gmail.com>
2018-12-05 09:06:24 +0900

    Add login function

--- packages/nuxt-blog-service/app/pages/index.vue
+++ packages/nuxt-blog-service/app/pages/index.vue
@@ -12,7 +12,7 @@
             <el-checkbox v-model="isCreateMode">アカウントを作成する</el-checkbox>
           </div>
           <div class="text-right">
-            <el-button typee="primary">{{ buttonText }}</el-button>
+            <el-button typee="primary" @click="handleClickSubmit">{{ buttonText }}</el-button>
           </div>
         </div>
       </form>
@@ -21,8 +21,13 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-  asyncData() {
+  asyncData({ redirect, store }) {
+    if (store.getters['user']) {
+      redirect('/posts/')
+    }
     return {
       isCreateMode: false,
       formData: {
@@ -34,7 +39,53 @@ export default {
   computed: {
     buttonText() {
       return this.isCreateMode ? '新規登録' : 'ログイン'
-    }
+    },
+    ...mapGetters(['user'])
+  },
+
+  methods: {
+    async handleClickSubmit() {
+      if (this.isCreateMode) {
+        try {
+          await this.register({ ...this.formData })
+          this.$notify({
+            type: 'success',
+            title: 'アカウント作成完了',
+            message: `${this.formData.id} として登録しました`,
+            position: 'bottom-right',
+            duration: 1000
+          })
+          this.$router.push('/posts/')
+        } catch (e) {
+          this.$notify.error({
+            title: 'アカウント作成失敗',
+            message: '既に登録されているか、不正なユーザーIDです',
+            duration: 1000
+          })
+        }
+      } else {
+        try {
+          await this.login({ ...this.formData })
+          this.$notify({
+            type: 'success',
+            title: 'ログイン成功',
+            message: `${this.formData.id} としてログインしました`,
+            position: 'bottom-right',
+            duration: 1000
+          })
+          this.$router.push('/posts/')
+        } catch (e) {
+          this.$notify.error({
+            title: 'ログイン失敗',
+            message: '不正なユーザーIDです',
+            position: 'bottom-right',
+            duration: 1000
+          })
+        }
+      }
+    },
+
+    ...mapActions(['login', 'register'])
   }
 }
 </script>
