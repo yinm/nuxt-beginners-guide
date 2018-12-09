2249c2075333347e73720cd7fef776c2ae0bdf41
yinm <yinm.cminor9@gmail.com>
2018-12-09 18:17:17 +0900

    Fix chore

--- packages/nuxt-blog-service/app/store/index.js
+++ packages/nuxt-blog-service/app/store/index.js
@@ -18,16 +18,16 @@ export const mutations = {
 export const actions = {
   async login({ commit }, { id }) {
     const user = await this.$axios.$get(`/users/${id}.json`)
-    if (user.id) throw new Error('Invalid user')
+    if (!user.id) throw new Error('Invalid user')
     commit('setUser', { user })
   },
 
   async register({ commit }, { id }) {
     const payload = {}
     payload[id] = { id }
-    await this.$axios.$patch(`/users.json`, payload)
+    await this.$axios.$patch('/users.json', payload)
     const user = await this.$axios.$get(`/users/${id}.json`)
-    if (user.id) throw new Error('Invalid user')
+    if (!user.id) throw new Error('Invalid user')
     commit('setUser', { user })
   }
 }
