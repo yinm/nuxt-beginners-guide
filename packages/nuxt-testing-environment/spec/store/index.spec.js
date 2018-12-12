e22f941ce434816a6495ecb9913bf07654c2f922
yinm <yinm.cminor9@gmail.com>
2018-12-12 09:25:36 +0900

    Add alias

--- packages/nuxt-testing-environment/spec/store/index.spec.js
+++ packages/nuxt-testing-environment/spec/store/index.spec.js
@@ -1,6 +1,6 @@
 // 2nd
 import Vuex from 'vuex'
-import index from '../../app/store'
+import index from '~/store'
 import { createLocalVue } from '@vue/test-utils'
 import cloneDeep from 'lodash.clonedeep'
 
