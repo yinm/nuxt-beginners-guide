ec7fc6a1a0ec09a90ffb89336f36c0f7e1b3fa22
yinm <yinm.cminor9@gmail.com>
2018-12-13 08:55:31 +0900

    Check sw

--- packages/modules-pwa-example/pages/index.vue
+++ packages/modules-pwa-example/pages/index.vue
@@ -1,38 +1,45 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        modules-pwa-example
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <div v-if="active">
+      <p v-if="isOnline" class="is-online">
+        ● Application is online
+      </p>
+      <p v-else class="is-offline">
+        ○ Application is offline
+      </p>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  data() {
+    return {
+      active: false,
+      isOnline: false
+    }
+  },
+
+  mounted() {
+    this.active = true
+    this.isOnline = navigator.onLine
   }
 }
 </script>
 
-<style>
+<style scoped>
+.is-online {
+  color: #62c082;
+  font-weight: bold;
+  font-size: 24px;
+}
+
+.is-offline {
+  color: #777;
+  font-weight: bold;
+  font-size: 24px;
+}
+
 .container {
   min-height: 100vh;
   display: flex;
@@ -40,26 +47,4 @@ export default {
   align-items: center;
   text-align: center;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
-
