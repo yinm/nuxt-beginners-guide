cc01a0ad7bcdf349fba6db16c3ed5bf2bcb82e11
yinm <yinm.cminor9@gmail.com>
2018-12-13 08:40:53 +0900

    Check proxy

--- packages/modules-proxy-example/pages/index.vue
+++ packages/modules-proxy-example/pages/index.vue
@@ -1,65 +1,37 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
         modules-proxy-example
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          {{ post.title }}
+        </li>
+      </ul>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  async mounted() {
+    this.posts = await this.$axios.$get('/api/posts')
   }
 }
 </script>
 
-<style>
+<style scoped>
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  margin: 16px;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+ul {
+  margin: 16px 0;
 }
 </style>
-
