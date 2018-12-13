754ac2da5e7a775691a5d56d8625ba33f89bf5cc
yinm <yinm.cminor9@gmail.com>
2018-12-14 08:33:29 +0900

    Add server middleware

--- packages/my-first-nuxt-module/lib/module.js
+++ packages/my-first-nuxt-module/lib/module.js
@@ -1,3 +1,10 @@
-module.exports = () => {}
+const middleware = (req, res, next) => {
+  console.log('server middleware')
+  next()
+}
+
+module.exports = function myModule() {
+  this.addServerMiddleware(middleware)
+}
 
 module.exports.meta = require('../package.json')
