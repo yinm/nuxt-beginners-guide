5dce68b0b3bd8e990014f696ee5c7cbc96f513d5
yinm <yinm.cminor9@gmail.com>
2018-12-14 08:36:37 +0900

    Update server middleware for redirect

--- packages/my-first-nuxt-module/lib/module.js
+++ packages/my-first-nuxt-module/lib/module.js
@@ -1,6 +1,15 @@
+const url = require('url')
+
 const middleware = (req, res, next) => {
-  console.log('server middleware')
-  next()
+  req.query = url.parse(req.url, true).query
+  const {redirect_to} = req.query || {}
+  if (!redirect_to || redirect_to[0] !== '/' || redirect_to.match(/(\.\.)/)) {
+    return next()
+  }
+  res.writeHead(302, {
+    Location: redirect_to
+  })
+  res.end()
 }
 
 module.exports = function myModule() {
